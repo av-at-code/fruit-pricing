10f557ef6f32ac09b1bf7b8b53521017cc899bf0
av-at-code <ausharia@yahoo.co.in>
2025-05-06 00:04:58 +0530

    formatting

--- src/computePrice.ts
+++ src/computePrice.ts
@@ -1,44 +1,38 @@
 import { pricingRules } from "./pricingRules";
 import { Fruit } from "./types";
 
-export function calculateTotal(cart: Fruit[]) : number {
-    const counts: Record<Fruit, number> = {
-        Apple: 0,
-        Banana: 0,
-        Melon: 0,
-        Lime: 0,
-    };
-    for(const item of cart) {
-        if(!(item in counts)) {
-            console.log('Unknown fruit, skipping');
-            continue;
-        }
-        counts[item]++;
+export function calculateTotal(cart: Fruit[]): number {
+  const counts: Record<Fruit, number> = {
+    Apple: 0,
+    Banana: 0,
+    Melon: 0,
+    Lime: 0,
+  };
+  for (const item of cart) {
+    if (!(item in counts)) {
+      console.log("Unknown fruit, skipping");
+      continue;
     }
-    let total = 0;
-    for(const item in counts) {
-        const quantity = counts[item as Fruit];
-        total+= computePrice(item as Fruit, quantity);
-    }
-    return total;
-   
-
+    counts[item]++;
+  }
+  let total = 0;
+  for (const item in counts) {
+    const quantity = counts[item as Fruit];
+    total += computePrice(item as Fruit, quantity);
+  }
+  return total;
 }
 function computePrice(fruit: string, quantity: number): number {
-    const rule = pricingRules[fruit as Fruit];
-    
-    switch(rule.offer) {
-        case 'bogo':
-            return rule.pricePerItem * Math.ceil(quantity / 2);
-        case 'threeForTwo':
-            return rule.pricePerItem * ( Math.floor(quantity / 3) * 2 + (quantity % 3))
-            default:
-                return rule.pricePerItem * quantity;
-    }
+  const rule = pricingRules[fruit as Fruit];
 
+  switch (rule.offer) {
+    case "bogo":
+      return rule.pricePerItem * Math.ceil(quantity / 2);
+    case "threeForTwo":
+      return (
+        rule.pricePerItem * (Math.floor(quantity / 3) * 2 + (quantity % 3))
+      );
+    default:
+      return rule.pricePerItem * quantity;
+  }
 }
-
-
-    
-
-
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import { calculateTotal } from "./computePrice";
 import { Fruit } from "./types";
 
-const cart: Fruit[] = ['Apple', 'Banana', 'Melon', 'Melon', 'Melon', 'Lime', 'Lime', 'Lime'];
+const cart: Fruit[] = [
+  "Apple",
+  "Banana",
+  "Melon",
+  "Melon",
+  "Melon",
+  "Lime",
+  "Lime",
+  "Lime",
+];
 console.log(calculateTotal(cart));
