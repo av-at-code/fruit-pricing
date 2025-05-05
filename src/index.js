8d4a907b4e0fabcd5755345bba3f867cba8ba958
av-at-code <ausharia@yahoo.co.in>
2025-05-06 00:00:48 +0530

    removed js files

--- src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const computePrice_1 = require("./computePrice");
-const cart = ['Apple', 'Apple', 'Banana', 'Melon', 'Melon', 'Melon', 'Melon', 'Lime', 'Lime', 'Lime'];
-console.log((0, computePrice_1.calculateTotal)(cart));
