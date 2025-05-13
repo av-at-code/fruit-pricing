a8decc037eea58905626510e3323bba5e18f4dbf
av-at-code <52691408+av-at-code@users.noreply.github.com>
2025-05-13 11:15:55 +0530

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-export type OfferType = "bogo" | "threeForTwo" | null;
+export type OfferType = "bogo" | "threeForTwo" | "buyFourGetSix" | null;
 export type Fruit = "Apple" | "Banana" | "Melon" | "Lime";
 export interface PricingRule {
   pricePerItem: number;
